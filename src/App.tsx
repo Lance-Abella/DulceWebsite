7d894e5113df2fbb0c5bc745a1b6bffbe77ef1c9
LIL BOSS <abellalance2@gmail.com>
2025-03-29 10:59:38 +0800

    Initial Configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import type { Component } from "solid-js";
+
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+
+const App: Component = () => {
+  return (
+    <>
+      <header></header>
+      <body></body>
+    </>
+  );
+};
+
+export default App;
